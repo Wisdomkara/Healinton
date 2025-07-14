11c57eef1db25e11a9a925e93f42a92bdd549570
Wisdom Kara <wiskara1@gmail.com>
2025-07-14 22:35:57 +0100

    adding analytics wih vercel npm

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/next';
+import { Toaster } from '@/components/ui/toaster';
+import { Toaster as Sonner } from '@/components/ui/sonner';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from '@/hooks/useAuth';
 import Layout from '@/components/Layout';
-import Index from "./pages/Index";
-import Auth from "./pages/Auth";
-import Dashboard from "./pages/Dashboard";
-import Blog from "./pages/Blog";
-import BlogPost from "./pages/BlogPost";
-import Premium from "./pages/Premium";
-import AboutUs from "./pages/AboutUs";
-import FAQ from "./pages/FAQ";
-import TermsOfService from "./pages/TermsOfService";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
-import HealthInsurance from "./pages/HealthInsurance";
-import Community from "./pages/Community";
-import Accessibility from "./pages/Accessibility";
-import DrugsPage from "./pages/DrugsPage";
-import NotFound from "./pages/NotFound";
+import Index from './pages/Index';
+import Auth from './pages/Auth';
+import Dashboard from './pages/Dashboard';
+import Blog from './pages/Blog';
+import BlogPost from './pages/BlogPost';
+import Premium from './pages/Premium';
+import AboutUs from './pages/AboutUs';
+import FAQ from './pages/FAQ';
+import TermsOfService from './pages/TermsOfService';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import HealthInsurance from './pages/HealthInsurance';
+import Community from './pages/Community';
+import Accessibility from './pages/Accessibility';
+import DrugsPage from './pages/DrugsPage';
+import NotFound from './pages/NotFound';
 
 const queryClient = new QueryClient();
 
